8eae31b194d1675bfb878c4d2d61054ec392df39
TobyJChappell <tchappell@chapman.edu>
2019-10-02 17:33:46 -0700

    Upload Assignment2, Lab2, and updated Cars database

---
+++ Assignment_2/toby_chappell_assignment2.sql
@@ -0,0 +1,60 @@
+--author: Toby Chappell
+--date: 10/7/19
+--assignment: Assignment 2
+
+--1.	Create a table called Customers with the following structure:
+--		Customer ID – this is a primary key
+--		Customer first name
+--		Customer last name
+--		Customer age
+CREATE TABLE Customers(
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	age INT,
+	PRIMARY KEY(id)
+);
+
+--2.	Insert 3 full customer records into the table
+INSERT INTO Customers VALUES(1, 'Abby', 'Wong', 49);
+INSERT INTO Customers VALUES(2, 'Iris', 'Henderson', 94);
+INSERT INTO Customers VALUES(3, 'Bob', 'Smith', 63);
+
+--3.	You gain a new customer but only know their first name is Jim, and their last name is Terf. Insert this customer into your table - don’t forget to give him a primary key
+INSERT INTO Customers VALUES(4, 'Jim', 'Terf', NULL);
+
+--4.	Update all customer’s names to be upper case
+UPDATE Customers
+SET first_name = UPPER(first_name);
+
+--5.	Create a table called Customer_Address with the following structure:
+--		Customer ID – this is a foreign key to the Customers table
+--		Street number
+--		Street name
+--		City
+--		State
+--		Zip
+CREATE TABLE Customer_Address(
+	id INT,
+	street_number INT,
+	street_name VARCHAR(50),
+	city VARCHAR(50),
+	state VARCHAR(50),
+	zip INT,
+	FOREIGN KEY(id) REFERENCES Customers(id)
+);
+
+--6.	Insert an address for each of the customers you made in step 2 – make sure to adhere to foreign key rules
+INSERT INTO Customer_Address VALUES(1, 1600, 'Clifton Rd.', 'Atlanta', 'GA', 30333);
+INSERT INTO Customer_Address VALUES(2, 1, 'University Dr.', 'Orange', 'CA', 92866);
+INSERT INTO Customer_Address VALUES(3, 2920, 'Zoo Dr.', 'San Diego', 'CA', 92101);
+
+--7.	Update Jim Terf’s customer ID to be something different
+UPDATE Customers
+SET id = 5
+WHERE UPPER(first_name) = 'JIM' AND UPPER(last_name) = 'TERF';
+--WHERE id = 4;
+
+--8.	Delete one of the records you made in step 2.
+DELETE FROM Customers
+WHERE id = 1;
---
+++ Lab_2/toby_chappell_lab2.sql
@@ -0,0 +1,109 @@
+--author: Toby Chappell
+--date: 9/27/19
+--assignment: Lab 2
+
+--1. How many records are in the cars data
+SELECT COUNT(*) as num_cars
+FROM cars;
+
+--2. How many cars originated in the US?
+SELECT COUNT(*) as num_us
+FROM cars
+WHERE origin = 'US';
+
+--3.	How many cars were built in years 70-75?
+SELECT COUNT(*) as num_70_75
+FROM cars
+WHERE year BETWEEN 70 AND 75;
+
+--4.	How many cars get more than 30 MPG?
+SELECT COUNT(*) as mpg_30
+FROM cars
+WHERE mpg > 30;
+
+--5.	Are there any cars with more horsepower than displacement? If so, what models?
+SELECT model
+FROM cars
+WHERE horsepower > displacement;
+
+--6.	How many car models have an MPG lower than 11?
+SELECT COUNT(*) as mpg_11
+FROM cars
+WHERE mpg < 11;
+
+--7.	Where were the cars from #6 built? (Hint: use a subquery)
+SELECT DISTINCT origin
+FROM cars
+WHERE mpg < 11;
+
+--8.	Which car manufacturer origin has the lowest average horsepower?
+SELECT origin, horsepower
+FROM cars
+GROUP BY origin
+ORDER BY horsepower;
+
+--9.	How many cars made in the US have MPG under 20?
+SELECT COUNT(*) as mpg_20
+FROM cars
+WHERE origin = 'US' AND mpg < 20;
+
+--10.	How much horsepower does a ford fiesta have?
+SELECT horsepower
+FROM cars
+WHERE model = 'ford fiesta';
+
+--11.	Which number of cylinders has the highest average HP?
+SELECT cylinders, horsepower
+FROM cars
+GROUP BY cylinders
+ORDER BY horsepower DESC;
+
+--12.	What countries have 4-cylinder cars?
+SELECT DISTINCT origin
+FROM cars
+WHERE cylinders = 4;
+
+--13.	How many cars that weigh under 2000 get an MPG lower than 30?
+SELECT COUNT(*) as weight_2000_mpg_30
+FROM cars
+WHERE weight < 2000 AND mpg < 30;
+
+--14.	How many cylinders do the cars that get over 40 MPG have?
+SELECT DISTINCT cylinders
+FROM cars
+WHERE mpg > 40;
+
+--15.	What countries did the cars in #13 originate in?
+SELECT DISTINCT origin
+FROM cars
+WHERE mpg < 30 AND weight < 2000;
+
+--16.	What is the percentage (out of all the data) of cars that originate in the US?
+SELECT COUNT(*)*100.0/(SELECT COUNT(*) FROM cars) as percent_us
+FROM cars
+WHERE origin = 'US';
+
+--17.	What is the percentage (out of all the data) of cars have 8 cylinders?
+SELECT COUNT(*)*100.0/(SELECT COUNT(*) FROM cars) as percent_8_cylinders
+FROM cars
+WHERE cylinders = 8;
+
+--18.	Write a query to return the average displacement per origin+cylinder combo. What is the result?
+SELECT origin, cylinders, displacement
+FROM cars
+GROUP BY origin, cylinders
+HAVING AVG(displacement);
+
+--19.	Write a query to only return the cylinders and average displacement, where the origin is Japan, from the query in #18. (Hint: use a subquery)
+SELECT cylinders, displacement
+FROM cars
+WHERE origin = 'Japan'
+GROUP BY cylinders
+HAVING AVG(displacement);
+
+--20.	Find the model of the car that has the max horsepower in the US. (Hint: use a subquery)
+SELECT model, horsepower
+FROM cars
+WHERE origin = 'US'
+GROUP BY model
+ORDER BY horsepower DESC;
